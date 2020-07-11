525fac75207b178c685ef51a5f54244ee124b20b
Dave van der Merwe <dave@skymail.co.za>
2020-07-11 12:41:12 +0200

    After change to class HealCommand

---
+++ workspace/Simple Command/src/com/dave/simplecommand/HealCommand.java
@@ -0,0 +1,27 @@
+package com.dave.simplecommand;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class HealCommand implements CommandExecutor {
+	
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args ) {
+		
+		Player player = (Player) sender;
+		
+		player.setHealth(20.0);
+		player.sendMessage("Healed");
+		
+		
+		
+		return false;
+	}
+	
+
+
+
+}
